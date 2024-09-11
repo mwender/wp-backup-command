19b1cce14dc352c9b085a6d485a7d78aa54d28c6
Michael Wender <michael@michaelwender.com>
2024-09-11 09:56:02 -0400

    Backing up from the parent directory

--- bin/wp-backup.php
+++ bin/wp-backup.php
@@ -1,4 +1,5 @@
 <?php
+
 if ( ! class_exists( 'WP_CLI' ) ) {
   return;
 }
@@ -39,6 +40,8 @@ class WP_Backup_Command {
    */
   private function backup_item( $slug, $type ) {
     $path = ( $type === 'plugin' ) ? WP_PLUGIN_DIR . "/$slug" : get_theme_root() . "/$slug";
+    $parent_dir = ( $type === 'plugin' ) ? WP_PLUGIN_DIR : get_theme_root();
+
     if ( ! file_exists( $path ) ) {
       WP_CLI::error( ucfirst( $type ) . " '$slug' not found." );
     }
@@ -53,10 +56,10 @@ class WP_Backup_Command {
     // Check if zip is installed
     if ( $this->is_command_available( 'zip' ) ) {
       WP_CLI::log( 'Creating zip backup...' );
-      $this->create_zip( $path, $backup_filename );
+      $this->create_zip( $slug, $parent_dir, $backup_filename );
     } elseif ( $this->is_command_available( 'tar' ) ) {
       WP_CLI::log( 'Creating tar.gz backup...' );
-      $this->create_tar_gz( $path, $backup_filename );
+      $this->create_tar_gz( $slug, $parent_dir, $backup_filename );
     } else {
       WP_CLI::error( 'Neither zip nor tar is available on the server.' );
     }
@@ -75,20 +78,26 @@ class WP_Backup_Command {
   /**
    * Create a zip archive.
    */
-  private function create_zip( $source, $filename ) {
+  private function create_zip( $slug, $parent_dir, $filename ) {
     $zipfile = $filename . '.zip';
-    $command = "zip -r $zipfile $source";
+
+    // Change to parent directory and create the zip with the relative path
+    $command = "cd $parent_dir && zip -r $zipfile $slug";
     shell_exec( $command );
+
     WP_CLI::log( "Backup created: $zipfile" );
   }
 
   /**
    * Create a tar.gz archive.
    */
-  private function create_tar_gz( $source, $filename ) {
+  private function create_tar_gz( $slug, $parent_dir, $filename ) {
     $tarfile = $filename . '.tar.gz';
-    $command = "tar -czvf $tarfile -C " . dirname( $source ) . " " . basename( $source );
+
+    // Change to parent directory and create the tar.gz with the relative path
+    $command = "cd $parent_dir && tar -czvf $tarfile $slug";
     shell_exec( $command );
+
     WP_CLI::log( "Backup created: $tarfile" );
   }
 }
